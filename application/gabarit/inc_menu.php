fc67986c02f2616fddc7ff637903d20c31bff93b
florian <florian.techer@outlook.com>
2026-06-18 12:00:47 +0200

    debugage du menu

--- application/gabarit/inc_menu.php
+++ application/gabarit/inc_menu.php
@@ -14,8 +14,8 @@
 <li><a class='nav-link' href='<?=hlien("commande","index")?>'>Commande</a></li>
 <li><a class='nav-link' href='<?=hlien("profil","index")?>'>Profil</a></li>
 <li><a class='nav-link' href='<?=hlien("utilisateur","index")?>'>Utilisateur</a></li>
-<?php } else if (isset($_SESSION["uti_profil"]) and $_SESSION["uti_profil"]==1) { ?>
-<li><a class='nav-link' href='<?=hlien("commande","panier","id",$_GET["id"])?>'>Commande</a></li>
+<?php } else if (isset($_SESSION["uti_id"]) and $_SESSION["uti_profil"]==1) { ?>
+<li><a class='nav-link' href='<?=hlien("commande","panier","id",$com_id)?>'>Commande</a></li>
       </ul>
       <?php } ?>
       <ul class="navbar-nav ml-auto">
